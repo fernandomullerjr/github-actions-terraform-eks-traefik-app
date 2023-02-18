bc792b76428977a592c83c75a6fd4c7f0e474cc6
Fernando Muller Junior <fernandomj90@gmail.com>
2023-02-18 18:55:29 -0300

    AULA 58. GitHub Actions - Terraform + EKS. TSHOOT, terraform apply

--- eks/eks-cluster.tf
+++ eks/eks-cluster.tf
@@ -3,7 +3,7 @@ module "eks" {
   version = "17.24.0"
 
   cluster_name    = local.cluster_name
-  cluster_version = "1.18"
+  cluster_version = "1.21"
   subnets         = module.vpc.public_subnets
   tags = {
     Environment = "training"
